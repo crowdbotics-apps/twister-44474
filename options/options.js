0dbcdb73163cf6281580e69ca050998b3d70dce2
Crowdbotics <team@crowdbotics.com>
2023-11-10 18:35:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twister-44474",
+  url: "https://twister-44474.botics.co",
+  api: "https://twister-44474.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
